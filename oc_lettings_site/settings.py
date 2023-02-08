0f0f5995c079dd982a8b0eb5011cc988d20f1c02
TiffanyCALVET <tiffany.calvet@developpement-durable.gouv.fr>
2023-02-08 16:29:25 +0100

    18 commit : update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://oc-lettings-p13.herokuapp.com', '0.0.0.0', '127.0.0.1']
 
 # Application definition
 
@@ -111,3 +111,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
