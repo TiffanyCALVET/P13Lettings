94e627d5d977950a591b349b779727b100734f76
TiffanyCALVET <tiffany.calvet@developpement-durable.gouv.fr>
2023-02-13 11:36:53 +0100

    22 commit pb collectstatic

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = []
 
 # Config Sentry :
 
